ce456977bb84ff514f8a235a5f04291dfca2c25e
hendricwidjaja <hcwidjaja@gmail.com>
2024-09-07 10:23:33 +1000

    Created authorise as admin decorator, created delete function for user

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from flask_jwt_extended import get_jwt_identity
 
+from functools import wraps
+
 from init import db
 from models.user import User
 
@@ -11,3 +13,23 @@ def authorise_as_admin():
     user = db.session.scalar(stmt)
     # check whether the user is an admin or not
     return user.is_admin
+
+# creating a decorator for authorise_as_admin
+
+def auth_as_admin_decorator(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        # get the user's id from get_jwt_identity
+        user_id = get_jwt_identity()
+        # fetch the user from the db
+        stmt = db.select(User).filter_by(id=user_id)
+        user = db.session.scalar(stmt)
+        # if user is admin
+        if user.is_admin:
+            # allow the decorator "fn" to execute - this is because the decorator 
+            return fn(*args, **kwargs)
+        # else
+        else:
+            return {"error": "Only admin can perform this action "}, 403
+        
+    return wrapper
